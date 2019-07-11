608ebe8ce57f4ab76b8d5a2195812ae54e98ff0e
Fabio Martins <fabiomartins1988@gmail.com>
2019-07-11 11:58:02 -0300

    Projeto SPringMVC com thymeleaf

---
+++ src/main/java/br/com/treinaweb/springboot/HelloSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+
+}
